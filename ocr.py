02a1e608a4bf1ae2501d104583d0dd7414d87f7a
Keerthi Priya Dwarampudi <dkeerthipriya6@gmail.com>
2026-09-22 19:06:16 +0530

    Configure Tesseract OCR

--- ocr.py
+++ ocr.py
@@ -1,16 +1,54 @@
 from PIL import Image
 import pytesseract
+import shutil
+import os
 
+# find the tesseract executable automatically and allows application to work on both(windows and linux)
+def configure_tesseract():
+    """
+    Find the Tesseract executable in the current environment.
 
-pytesseract.pytesseract.tesseract_cmd = (
-    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-)
-
+    Works both locally on Windows and on Linux deployment
+    environments such as Render.
+    """
+    # First allow an explicit environment variable
+    tesseract_path = os.getenv("TESSERACT_CMD")
+    # Otherwise find Tesseract from PATH
+    if not tesseract_path:
+        tesseract_path = shutil.which("tesseract")
+    if not tesseract_path:
+        raise RuntimeError(
+            "Tesseract OCR is not installed or is not available "
+            "in the system PATH."
+        )
+    pytesseract.pytesseract.tesseract_cmd = tesseract_path
 
 def extract_text_from_image(file):
-
+    configure_tesseract()
     image = Image.open(file)
-
-    text = pytesseract.image_to_string(image)
-
+    text = pytesseract.image_to_string(
+        image,
+        lang="eng"
+    )
     return text
+
+
+
+
+
+# from PIL import Image
+# import pytesseract
+
+
+# pytesseract.pytesseract.tesseract_cmd = (
+#     r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# )
+
+
+# def extract_text_from_image(file):
+
+#     image = Image.open(file)
+
+#     text = pytesseract.image_to_string(image)
+
+#     return text
